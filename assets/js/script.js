ebad82ae4398d79c3740d74d3ce5cea83806c3ba
mihan007 <vVeZ4A8E>
2015-04-23 15:30:08 +0300

    more accurate site now

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,7 @@
         if ($('.countdown').length > 0) {
             $('.countdown').final_countdown({
                 start: '1427220000',
-                end: '1429898400',
+                end: '1429880400',
                 now: Math.floor(Date.now() / 1000),
                 seconds: {
                     borderColor: '#FFF',
